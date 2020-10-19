a7987526f0f4d159d99d4f20481c6db8c26a016d
damokless <damokless@protonmail.com>
2020-10-19 08:53:57 +0200

    add e01.js

---
+++ e01.js
@@ -0,0 +1,22 @@
+function bubble(myArray = []) {
+    
+    let finish
+    let temp
+
+    do {
+        finish = false;
+        for (let i = 0; i < myArray.length; i++) {
+            
+            if (myArray[i] > myArray[i+1]) {
+                temp = myArray[i]
+                myArray[i] = myArray[i+1]
+                myArray[i+1] = temp
+
+                finish = true
+            }
+        }
+    } while (finish);
+return myArray
+}
+
+console.log(bubble([5,1,4,2,8]))
